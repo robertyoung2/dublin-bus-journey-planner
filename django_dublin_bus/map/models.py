e9dffadb7341aabaab692f27d125886a8d33043a
Robert Young <robert.young.ie@gmail.com>
2019-06-25 19:45:48 +0100

    First commit of basic Django framework

---
+++ django_dublin_bus/map/models.py
@@ -0,0 +1,30 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey has `on_delete` set to the desired behavior.
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
+from django.db import models
+
+
+class BusStops(models.Model):
+    stop_id = models.TextField(blank=True, null=False, primary_key=True)
+    bus_numbers = models.TextField(blank=True, null=True)
+    stop_headsign = models.TextField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'bus_stops'
+
+
+class StopsInfo(models.Model):
+    stop_lat = models.FloatField(blank=True, null=True)
+    stop_lon = models.FloatField(blank=True, null=True)
+    stop_id = models.TextField(blank=True, null=False, primary_key=True)
+    actual_stop_id = models.IntegerField(blank=True, null=True)
+    stop_name = models.TextField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'stops_info'
