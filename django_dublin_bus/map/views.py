445568b1bfd9fbf15bf5805fd7ba688b126d9200
Robert Young <robert.young.ie@gmail.com>
2019-07-02 19:59:29 +0100

    Pure Django Map + Marker implementation

--- django_dublin_bus/map/views.py
+++ django_dublin_bus/map/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import StopsInfo
 # Create your views here.
 
 def home(request):
-    return render(request, 'map/home.html')
+    bus_stops = StopsInfo.objects.all()
+    context = {'bus_stops': bus_stops}
+    return render(request,'map/home.html', context)
+
