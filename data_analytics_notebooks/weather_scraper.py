db675f9a502ea0135696b4e41504fc7bc6398217
Robert Young <robert.young.ie@gmail.com>
2019-08-06 15:04:54 +0100

    Weather scraper update for sunrise and sunset

--- data_analytics_notebooks/weather_scraper.py
+++ data_analytics_notebooks/weather_scraper.py
@@ -14,22 +14,38 @@ try:
     response = requests.get('https://api.darksky.net/forecast/95ab53fceccb16cedea7ed90b54c167d/53.3498,-6.2603?'
                             'extend=hourly&exclude=minutely,alerts,flags&units=auto')
     weather_data = response.json()
+
     df = pd.DataFrame(columns=['ts_recorded_data', 'ts_unix_recorded_data', 'ts_weather', 'ts_unix_weather',
                                'rain_mm', 'temp_c'],
                           index = range(0,169))
 
+    df_sunset_sunrise = pd.DataFrame(columns=['ts_recorded_data', 'date', 'ts_sunrise', 'ts_sunset'],
+                                     index=range(0, 8))
+
+    current_time = calendar.timegm(time.gmtime())
+
     for item_num in range(169):
-        current_time = calendar.timegm(time.gmtime())
-        df.iloc[item_num]['ts_unix_recorded_data'] = current_time
-        df.iloc[item_num]['ts_recorded_data'] = datetime.datetime.utcfromtimestamp(current_time).strftime('%Y-%m-%d'
-                                                                                                          ' %H:%M:%S')
         ts_weather = int(weather_data['hourly']['data'][item_num]['time'])
         df.iloc[item_num]['ts_weather'] = datetime.datetime.utcfromtimestamp(ts_weather).strftime('%Y-%m-%d %H:%M:%S')
         df.iloc[item_num]['ts_unix_weather'] = ts_weather
         df.iloc[item_num]['temp_c'] = (weather_data['hourly']['data'][item_num]['temperature'])
         df.iloc[item_num]['rain_mm'] = (weather_data['hourly']['data'][item_num]['precipIntensity'])
 
-    df.to_csv('168_hours_weather.csv', header=True)
+    df['ts_unix_recorded_data'] = current_time
+    df['ts_recorded_data'] = datetime.datetime.utcfromtimestamp(current_time).strftime('%Y-%m-%d %H:%M:%S')
+    df.to_csv('168_hours_weather.csv', header=True, index=False)
+
+    for item_num in range(8):
+        ts_sunrise = (weather_data['daily']['data'][item_num]['sunriseTime'])
+        ts_sunset = (weather_data['daily']['data'][item_num]['sunsetTime'])
+        df_sunset_sunrise.iloc[item_num]['ts_sunrise'] = datetime.datetime.utcfromtimestamp(ts_sunrise).strftime(
+            '%Y-%m-%d %H:%M:%S')
+        df_sunset_sunrise.iloc[item_num]['ts_sunset'] = datetime.datetime.utcfromtimestamp(ts_sunset).strftime(
+            '%Y-%m-%d %H:%M:%S')
+        df_sunset_sunrise.iloc[item_num]['date'] = datetime.datetime.utcfromtimestamp(ts_sunrise).strftime('%Y-%m-%d')
+        df_sunset_sunrise['ts_recorded_data'] = datetime.datetime.utcfromtimestamp(current_time).strftime(
+            '%Y-%m-%d %H:%M:%S')
+        df_sunset_sunrise.to_csv('sunrise_sunset.csv', header=True, index=False)
 
 except:
     f = open("logTracebackError.log", "a+")
