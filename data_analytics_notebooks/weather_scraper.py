cf0aeae7bff2d9f5ebfed94a1b2a78bf18bc90ed
Robert Young <robert.young.ie@gmail.com>
2019-08-06 11:05:49 +0100

    Update for PEP8 line length

--- data_analytics_notebooks/weather_scraper.py
+++ data_analytics_notebooks/weather_scraper.py
@@ -14,13 +14,15 @@ try:
     response = requests.get('https://api.darksky.net/forecast/95ab53fceccb16cedea7ed90b54c167d/53.3498,-6.2603?'
                             'extend=hourly&exclude=minutely,alerts,flags&units=auto')
     weather_data = response.json()
-    df = pd.DataFrame(columns=['ts_recorded_data', 'ts_unix_recorded_data', 'ts_weather', 'ts_unix_weather', 'rain_mm', 'temp_c'],
+    df = pd.DataFrame(columns=['ts_recorded_data', 'ts_unix_recorded_data', 'ts_weather', 'ts_unix_weather',
+                               'rain_mm', 'temp_c'],
                           index = range(0,169))
 
     for item_num in range(169):
-        current_time =  calendar.timegm(time.gmtime())
+        current_time = calendar.timegm(time.gmtime())
         df.iloc[item_num]['ts_unix_recorded_data'] = current_time
-        df.iloc[item_num]['ts_recorded_data'] = datetime.datetime.utcfromtimestamp(current_time).strftime('%Y-%m-%d %H:%M:%S')
+        df.iloc[item_num]['ts_recorded_data'] = datetime.datetime.utcfromtimestamp(current_time).strftime('%Y-%m-%d'
+                                                                                                          ' %H:%M:%S')
         ts_weather = int(weather_data['hourly']['data'][item_num]['time'])
         df.iloc[item_num]['ts_weather'] = datetime.datetime.utcfromtimestamp(ts_weather).strftime('%Y-%m-%d %H:%M:%S')
         df.iloc[item_num]['ts_unix_weather'] = ts_weather
