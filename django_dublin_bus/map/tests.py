2caa9382e95b01afc4453923690379d91e1566a9
Robert Young <robert.young.ie@gmail.com>
2019-08-06 19:25:22 +0100

    Django unit tests

--- django_dublin_bus/map/tests.py
+++ django_dublin_bus/map/tests.py
@@ -1,3 +1,33 @@
-from django.test import TestCase
+import unittest
+from django.test import Client
+from django.urls import reverse
 
-# Create your tests here.
+class MapUnitTesting(unittest.TestCase):
+
+    # Set up the client
+    def setUp(self):
+        self.client = Client()
+
+    def test_details(self):
+        # Issue a GET request.
+        url = reverse("map-home")
+        response = self.client.get(url)
+
+        # Check that the response is 200 OK.
+        self.assertEqual(response.status_code, 200)
+
+        # Check that the rendered context returns all stops from databse.
+        self.assertEqual(len(response.context['bus_stops']), 4232)
+
+    
+
+
+
+
+
+
+
+
+
+
+
