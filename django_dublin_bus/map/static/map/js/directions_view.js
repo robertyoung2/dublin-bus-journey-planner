0d4507d724a576d199978ae66039625e48f1bba1
Robert Young <robert.young.ie@gmail.com>
2019-08-14 16:52:58 +0100

    Grid layout for text input tab 1

--- django_dublin_bus/map/static/map/js/directions_view.js
+++ django_dublin_bus/map/static/map/js/directions_view.js
@@ -16,99 +16,58 @@ function generate_directions_views(){
 
         if(directions_section.innerHTML === ""){
             directions_section.innerHTML =`
-                <form class="cell" action="Getinput" method="get">
-                        <div class="grid-x cell align-center">
-                        
-<!--                            Origin Input-->
-                            <div class="cell small-8 medium-8">
-                                    <div class="mdl-textfield mdl-js-textfield">
-                                        <input class="mdl-textfield__input" type="text" name="input_route_origin" id="input_route_origin" placeholder = "Enter Origin (Default Current Location)">
-                                        <label class="mdl-textfield__label" for="input_route_origin"></label>
-                                    </div>
-                            </div>
-                            
- 
-                            <div class="cell small-2 medium-2">
-                                <a tabindex="-1" onclick="clearSearch('input_route_origin')"><img id="clear_search_icon" title="Clear origin search" src="/static/map/images/baseline-clear-24px.svg"></a>
-                            </div>
-                            
-                            <!--                            Destination Input-->
-                            <div class="cell small-8 medium-8">
-                                    <div class="mdl-textfield mdl-js-textfield">
-                                        <input class="mdl-textfield__input" type="text" name="input_route_destination" id="input_route_destination" placeholder="Enter Destination">
-                                        <label class="mdl-textfield__label" for="input_route_destination"></label>
-                                    </div>
-                            </div>
-                        
-     
-                            <div class="cell small-2 medium-2">
-                                <a tabindex="-1" onclick="clearSearch('input_route_destination')"><img id="clear_search_icon" title="Clear destination search" src="/static/map/images/baseline-clear-24px.svg"></a>
-                            </div>
-                        </div>
 
-                        <div class="grid-x cell align-center">
-                            <div class="cell small-6 medium-6 large-4">
-                            
-                            
-                            
-                                <select id="option" onchange="set_date_options()">
-                                    <option value="now">Now</option>
-                                    <option value="departureTime">Departure Time</option>
-                                    <option value="arrivalTime">Arrival Time</option>
-                                </select>
-                            </div>
+<form class="cell" action="Getinput" method="get">
 
-                            <div class="cell small-4 medium-4 large-6"></div>
-                        </div>
-    
-                        <!--                            Destination Input-->
-                        <div class="cell small-8 medium-8">
-                            <form action="#">
-                                <div class="mdl-textfield mdl-js-textfield">
-                                    <input class="mdl-textfield__input" type="text" name="input_route_destination" id="input_route_destination" placeholder="Enter Destination">
-                                    <label class="mdl-textfield__label" for="input_route_destination"></label>
-                                </div>
-                            </form>
-                        </div>
-    
-    
-                        <div class="cell small-2 medium-2">
-                            <a tabindex="-1" onclick="clearSearch('input_route_destination')"><img id="clear_search_icon" title="Clear destination search" src="/static/map/images/baseline-clear-24px.svg"></a>
-                        </div>
-                    </div>
-    
-                    <div class="grid-x cell align-center">
-                        <div class="cell small-6 medium-6 large-4">
-                            <select id="option">
-                                <option value="now">Now</option>
-                                <option value="departureTime">Departure Time</option>
-                                <option value="arrivalTime">Arrival Time</option>
-                            </select>
-                        </div>
-    
-                        <div class="cell small-4 medium-4 large-6"></div>
-                    </div>
-                    <div id="datetime_selector_container" class="grid-x cell align-center" style="display: none">
-                        <div class="small-6 medium-6 cell">
-                            <select id="date"></select>
-                        </div>
-                        <div class="small-3 medium-3 cell">
-                            <input type="time" id="journey_time" value="now" required>
-                        </div>
-                        <div class="small-1 medium-1 cell"></div>
-                    </div>
-                    <div class="grid-x cell align-center">
-    
-    
-                        <div class="small-3 medium-3 cell">
-                            <input type="button" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" id="route_submit" onclick="geocodeAddress()" value="Search">
-    <!--                                <input type="button" id="route_submit" onclick="geocodeAddress()" value="Search">-->
-                        </div>
-    
+<!--    Origin Input-->
 
-                        <div class="small-7 medium-7 cell"></div>
-                    </div>
-                </form>
+    <div class="mdl-grid">
+        <div class="mdl-cell mdl-cell--7-col mdl-cell--5-col-tablet mdl-cell--3-col-phone">
+            <div class="mdl-textfield mdl-js-textfield ">
+                <input class="mdl-textfield__input" type="text" name="input_route_origin" id="input_route_origin" placeholder="Enter Origin (Default Current Location)">
+                <label class="mdl-textfield__label" for="input_route_origin"></label>
+            </div>
+        </div>
+    
+        <div class="mdl-cell mdl-cell--5-col mdl-cell--3-col-tablet mdl-cell--1-col-phone mdl-cell--middle">
+            <a tabindex="-1" onclick="clearSearch('input_route_origin')"><img id="clear_search_icon" title="Clear origin search" src="/static/map/images/baseline-clear-24px.svg"></a>
+        </div>
+        
+    </div>
+
+    <!--                            Destination Input-->
+    <div class="mdl-grid">
+        <div class="mdl-cell mdl-cell--7-col mdl-cell--5-col-tablet mdl-cell--3-col-phone">
+            <div class="mdl-textfield mdl-js-textfield">
+                <input class="mdl-textfield__input" type="text" name="input_route_destination" id="input_route_destination" placeholder="Enter Destination">
+                <label class="mdl-textfield__label" for="input_route_destination"></label>
+             </div>
+         </div>
+             
+        <div class="mdl-cell mdl-cell--5-col mdl-cell--3-col-tablet mdl-cell--1-col-phone mdl-cell--middle">
+             <a tabindex="-1" onclick="clearSearch('input_route_destination')"><img id="clear_search_icon" title="Clear destination search" src="/static/map/images/baseline-clear-24px.svg"></a>
+        </div>
+        
+    </div>
+
+<!--    <select id="option" onchange="set_date_options()">-->
+<!--        <option value="now">Now</option>-->
+<!--        <option value="departureTime">Departure Time</option>-->
+<!--        <option value="arrivalTime">Arrival Time</option>-->
+<!--    </select>-->
+
+
+    <!--                    <div id="datetime_selector_container" class="grid-x cell align-center" style="display: none">-->
+
+<!--    <select id="date"></select>-->
+
+
+<!--    <input type="time" id="journey_time" value="now" required>-->
+
+<!--    <input type="button" class="mdl-button mdl-js-button mdl-button&#45;&#45;raised mdl-js-ripple-effect" id="route_submit" onclick="geocodeAddress()" value="Search">-->
+
+</form>
+
                 `;
             generateRouteSearch();
             set_date_options();
