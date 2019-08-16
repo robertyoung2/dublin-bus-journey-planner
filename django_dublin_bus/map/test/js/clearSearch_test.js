3b09fc71a9c3c46fd4099dc75ec27b5423f7aff6
Conor Lonergan <conorlonergan91@gmail.com>
2019-08-16 20:45:26 +0100

    Fixed Unit Test

--- django_dublin_bus/map/test/js/clearSearch_test.js
+++ django_dublin_bus/map/test/js/clearSearch_test.js
@@ -2,8 +2,8 @@
 QUnit.test( "Testing of clear search", function( assert ) {
     //Call the function to see if it erase the element's value.
     let element_id = 'set_location';
-    document.getElementById(element_id).value = "StillOrgan";
     let empty_box= document.getElementById(element_id).value;
+    document.getElementById(element_id).value = "StillOrgan";
 
     clearSearch(element_id);
 
